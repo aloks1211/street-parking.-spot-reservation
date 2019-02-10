c8328c1c4d4812561a54d3d96bd3799756cb1c7a
aloks1211 <mail4aloksharma@gmail.com>
2019-02-10 21:33:39 +0530

    adding more views

---
+++ streetparkingspotreservation/reservation/tests/test_models.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+
+class TestReservationsViews(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def setUp(self):
+        pass
---
+++ streetparkingspotreservation/reservation/tests/test_views.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+
+class TestReservationsModels(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def setUp(self):
+        pass
